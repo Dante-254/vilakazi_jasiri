8f5cf4f23e09fa566fd6ff84093ddcc62c9cb83c
Dante-254 <giciadaniel575@gmail.com>
2026-01-05 15:24:37 +0300

    TEMplates_to_the_links_header

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    # path("contact/", views.contact, name="contact"),
+    path("contact/", views.contact, name="contact"),
+    path("structure/kenya-scouts-law/", views.kenya_scouts_law, name="kenya_scouts_law"),
+    path("structure/leadership/", views.leadership, name="leadership"),
+    path("what-we-do/news/", views.news, name="news"),
+    path("what-we-do/upcoming-events/", views.upcoming_events, name="upcoming_events"),
+    path("what-we-do/past-events/", views.past_events, name="past_events"),
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,21 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def kenya_scouts_law(request):
+    return render(request, "kenya_scouts_law.html")
+
+def leadership(request):
+    return render(request, "leadership.html")
+
+def news(request):
+    return render(request, "news.html")
+
+def upcoming_events(request):
+    return render(request, "upcoming_events.html")
+
+def past_events(request):
+    return render(request, "past_events.html")
